87337634b213b209f67785893429821ff3ea429b
Archie G <acgelonga@gmail.com>
2022-06-12 22:29:50 -0500

    Upload project files

--- js/_script.js
+++ js/_script.js
@@ -11,7 +11,6 @@ var audioFiles = [
   "Gb1.mp3",
   "G1.mp3",
   "Ab1.mp3",
-
   "A1.mp3",
   "Bb1.mp3",
   "B1.mp3",
@@ -24,7 +23,6 @@ var audioFiles = [
   "Gb2.mp3",
   "G2.mp3",
   "Ab2.mp3",
-
   "A2.mp3",
   "Bb2.mp3",
   "B2.mp3",
@@ -37,7 +35,6 @@ var audioFiles = [
   "Gb3.mp3",
   "G3.mp3",
   "Ab3.mp3",
-
   "A3.mp3",
   "Bb3.mp3",
   "B3.mp3",
@@ -50,7 +47,6 @@ var audioFiles = [
   "Gb4.mp3",
   "G4.mp3",
   "Ab4.mp3",
-
   "A4.mp3",
   "Bb4.mp3",
   "B4.mp3",
@@ -63,7 +59,6 @@ var audioFiles = [
   "Gb5.mp3",
   "G5.mp3",
   "Ab5.mp3",
-
   "A5.mp3",
   "Bb5.mp3",
   "B5.mp3",
@@ -76,7 +71,6 @@ var audioFiles = [
   "Gb6.mp3",
   "G6.mp3",
   "Ab6.mp3",
-
   "A6.mp3",
   "Bb6.mp3",
   "B6.mp3",
@@ -89,15 +83,41 @@ var audioFiles = [
   "Gb7.mp3",
   "G7.mp3",
   "Ab7.mp3",
-
   "A7.mp3",
   "Bb7.mp3",
   "B7.mp3",
   "C8.mp3",
 ];
 
-var audioElements = [];
-for (let i = 0; i < audioFiles.length; i++) {
-  let audioElement = new Audio(`audio/${audioFiles[i]}`);
-  audioElements.push(audioElement);
-}
+// var audioElements = [];
+// for (let i = 0; i < audioFiles.length; i++) {
+//   let audioElement = new Audio(`audio/${audioFiles[i]}`);
+//   audioElements.push(audioElement);
+// }
+
+var sequence = [
+  new Audio(`audio/${audioFiles[55]}`),
+  new Audio(`audio/${audioFiles[54]}`),
+  new Audio(`audio/${audioFiles[55]}`),
+  new Audio(`audio/${audioFiles[54]}`),
+  new Audio(`audio/${audioFiles[55]}`),
+];
+
+// document.addEventListener("click", function () {
+//   setTimeout(function () {
+//     sequence[0].play();
+//   }, 0);
+//   setTimeout(function () {
+//     sequence[1].play();
+//   }, 300);
+//   setTimeout(function () {
+//     sequence[2].play();
+//   }, 600);
+//   setTimeout(function () {
+//     sequence[3].play();
+//   }, 900);
+//   setTimeout(function () {
+//     sequence[4].playbackRate = 0.5;
+//     sequence[4].play();
+//   }, 1200);
+// });
